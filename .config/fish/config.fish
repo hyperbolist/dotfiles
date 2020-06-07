3d245960c555533ae1cb07fc75b4023bdb506d7d
Eric Sherman <hyperbolist@gmail.com>
2020-06-07 12:30:00 -0400

    moves user paths into ~/.fish_variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,16 +3,6 @@ if [ ! (which node) ] && [ -n .nvmrc ]
     nvm
 end
 
-# add python to path (yuck)
-# we do this almost exclusively for ranger
-set PATH /Library/Frameworks/Python.framework/Versions/3.7/bin $PATH
-
-# for kubectl krew
-set PATH $HOME/.krew/bin $PATH
-
-# add go to the path
-set PATH $HOME/go/bin $PATH
-
 # sensible aliases
 source ~/.fish_aliases
 
