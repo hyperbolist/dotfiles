99df6de37874e4ab99a5f1c9c96b1696675bd90c
Eric Sherman <hyperbolist@gmail.com>
2020-06-18 12:12:08 -0400

    adds brew bundle --global overload and .Brewfile

---
+++ .config/fish/functions/brew.fish
@@ -0,0 +1,9 @@
+function brew -a cmd -d 'brew - The Missing Package Manager for macOS'
+  switch "$cmd"
+    case bundle
+      command brew bundle --global --all --no-lock --describe $argv[2..99]
+
+    case '*'
+      command brew $argv
+  end
+end
