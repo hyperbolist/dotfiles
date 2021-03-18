208da81e0588f3c06fec3b2fc53433f03ac72ea0
Eric Sherman <hyperbolist@gmail.com>
2021-03-18 15:51:32 -0400

    nvim-tree git status colors working

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -1,5 +1,6 @@
 vim.g.base16colorspace=256
-vim.cmd('colorscheme base16-tomorrow')
+vim.o.termguicolors = true
+vim.cmd('colorscheme base16-tomorrow-night')
 
 -- moved highlights into packer setup for colorscheme plugin
 -- there's something strange happening with the 0.5 nightlies
--- .config/nvim/lua/nv-nvimtree/init.lua
+++ .config/nvim/lua/nv-nvimtree/init.lua
@@ -3,6 +3,9 @@ vim.g.nvim_tree_indent_markers = 1 --"0 by default, this option shows indent mar
 vim.g.nvim_tree_follow = 1 --"0 by default, this option allows the cursor to be updated when entering a buffer
 vim.g.nvim_tree_auto_close = 1 --0 by default, closes the tree when it's the last window
 vim.g.nvim_tree_auto_ignore_ft = 'startify' --"empty by default, don't auto open tree on specific filetypes.
+vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
+vim.g.nvim_tree_ignore = { '.git', 'node_modules' } -- empty by default
+vim.g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 vim.g.nvim_tree_bindings = {
     -- mappings
@@ -35,3 +38,4 @@ vim.g.nvim_tree_bindings = {
     ["q"] = tree_cb("close")
 }
 vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.cmd([[source ~/.config/nvim/vimscript/nv-nvimtree/init.vim]])
