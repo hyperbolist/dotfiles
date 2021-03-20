aca2d42d1eb42c9955d6c6532baafb51dad88fa4
Eric Sherman <hyperbolist@gmail.com>
2021-03-19 23:08:42 -0400

    autoclose explorer

--- .config/nvim/lua/nv-nvimtree/init.lua
+++ .config/nvim/lua/nv-nvimtree/init.lua
@@ -2,6 +2,7 @@ vim.g.nvim_tree_hide_dotfiles = 1 --0 by default, this option hides files and fo
 vim.g.nvim_tree_indent_markers = 1 --"0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_follow = 1 --"0 by default, this option allows the cursor to be updated when entering a buffer
 vim.g.nvim_tree_auto_close = 1 --0 by default, closes the tree when it's the last window
+vim.g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when it opens a file
 vim.g.nvim_tree_auto_ignore_ft = 'startify' --"empty by default, don't auto open tree on specific filetypes.
 vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
 vim.g.nvim_tree_ignore = { '.git', 'node_modules' } -- empty by default
