97320bca68e1b006bced8a428beaa1452dc9305d
Eric Sherman <hyperbolist@gmail.com>
2021-10-23 03:45:12 -0400

    minor lvim updates

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -11,6 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
+--lvim.colorscheme = "onedarker"
 lvim.colorscheme = "base16-tomorrow-night"
 lvim.transparent_window = true
 vim.opt.relativenumber = true
@@ -19,6 +20,7 @@ vim.opt.undofile = false
 lvim.lsp.document_highlight = false
 vim.opt.shell = "bash" -- let vim use bash for max compatibility, fish breaks noninteractive flows
 vim.opt.inccommand = "split" -- show live previews of substitutions
+vim.opt.cmdheight = 1 -- 2 felt like a strange default to me
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
@@ -30,19 +32,34 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- lvim.builtin.telescope.on_config_done = function()
---   local actions = require "telescope.actions"
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+-- local _, actions = pcall(require, "telescope.actions")
+-- lvim.builtin.telescope.defaults.mappings = {
 --   -- for input mode
---   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
---   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+--   i = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--     ["<C-n>"] = actions.cycle_history_next,
+--     ["<C-p>"] = actions.cycle_history_prev,
+--   },
 --   -- for normal mode
---   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
--- end
+--   n = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--   },
+-- }
 
 -- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+-- }
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
   name = "+Trouble",
@@ -60,11 +77,25 @@ lvim.builtin.which_key.mappings["t"] = {
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.side = "left"
+lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
+-- lvim.builtin.treesitter.ensure_installed = {
+--   "bash",
+--   "c",
+--   "javascript",
+--   "json",
+--   "lua",
+--   "python",
+--   "typescript",
+--   "css",
+--   "rust",
+--   "java",
+--   "yaml",
+-- }
 lvim.builtin.treesitter.ensure_installed = "maintained"
+
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
@@ -91,7 +122,7 @@ lvim.builtin.bufferline.on_config_done = function ()
     ]], false)
 end
 
-local components = require "core.lualine.components"
+local components = require "lvim.core.lualine.components"
 lvim.builtin.lualine.style = "none"
 lvim.builtin.lualine.options = {
   icons_enabled = true,
@@ -141,6 +172,7 @@ lvim.builtin.lualine.options.theme = "powerline"
 
 lvim.builtin.telescope.defaults.layout_config = {
   preview_cutoff = 10,
+  width = 0.9,
 }
 
 -- generic LSP settings
@@ -202,6 +234,7 @@ lvim.lang.python.linters = {
 --       cmd = "TroubleToggle",
 --     },
 -- }
+
 lvim.plugins = {
   {"RRethy/nvim-base16"},
   {"kevinhwang91/nvim-bqf"},
