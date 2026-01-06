e6e80010fe574c83d8b768a8ec6b65d2b0dd2630
Eric Sherman <hyperbolist@gmail.com>
2026-01-06 18:26:05 -0500

    adds asdf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -26,3 +26,17 @@ fish_vi_key_bindings
 set fish_cursor_insert block
 set fish_cursor_command block
 set fish_cursor_visual block
+
+# ASDF configuration code
+if test -z "$ASDF_DATA_DIR"
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
