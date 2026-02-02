e97ba71e36335ffcb2e1c13eba43dddb8744edcc
Eric Sherman <hyperbolist@gmail.com>
2026-02-02 16:16:43 -0500

    krew

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -40,3 +40,6 @@ if not contains $_asdf_shims $PATH
     set -gx --prepend PATH $_asdf_shims
 end
 set --erase _asdf_shims
+
+# krew
+set -q KREW_ROOT; and set -gx PATH $PATH $KREW_ROOT/.krew/bin; or set -gx PATH $PATH $HOME/.krew/bin
